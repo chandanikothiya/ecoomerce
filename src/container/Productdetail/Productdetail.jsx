f427d81a16b0431c95dcadfb46d1b0a6feceba15
chandanikothiya <kothiyachandani34@gmail.com>
2026-04-22 22:05:14 +0530

    ss

--- src/container/Productdetail/Productdetail.jsx
+++ src/container/Productdetail/Productdetail.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Box, Breadcrumbs, Button, Card, CardActions, CardContent, CardMedia, createTheme, FormControlLabel, Grid, IconButton, Radio, RadioGroup, Rating, Stack, ThemeProvider, Typography } from "@mui/material";
 import { PiLineVerticalThin } from "react-icons/pi";
 import { CiHeart } from "react-icons/ci";
@@ -11,6 +11,7 @@ import { Link, useParams } from "react-router-dom";
 import Divider from '@mui/material/Divider';
 import { useGetProductQuery } from "../../redux/api/product.api";
 import { IMG_URL } from "../../utility/url";
+
 
 function Productdetail() {
 
@@ -118,20 +119,20 @@ function Productdetail() {
                         <Typography sx={{ color: 'text.primary' }} className="breadcrumbs-typo">Havic HV G-92 Gamepad</Typography>
                     </Breadcrumbs>
 
-                    <Grid container sx={{ mt: { xs: 3, sm: 5, lg: 10 } }} spacing={{ xs: 3, sm: 5, lg: 6 }}>
-                        <Grid size={{ xs: 12, sm: 6, lg: 7 }} container spacing={{ xs: 2, sm: 3, xl: 3 }} alignItems="stretch">
-                            <Grid size={{ xs: 3, sm: 4, md: 3 }} sx={{
+                     <Grid container sx={{ mt: { xs: 3, sm: 5, lg: 10 } }} spacing={{ xs: 3, sm: 2, md: 5, lg: 6 }}>
+                        <Grid size={{ xs: 12, sm: 6, lg: 7 }} container spacing={{ xs: 2, sm: 2, md: 3, lg: 3.5, xl: 4 }} alignItems="stretch">
+                            <Grid size={{ xs: 3, sm: 3, md: 3 }} sx={{
                                 // flexDirection: {
                                 //     xs: 'row',     // mobile → row
                                 //     md: 'column'   // desktop → column
                                 // }
-                            }} container spacing={{ sm: 2, md: 4 }} alignSelf="flex-start">
+                            }} container spacing={{ sm: 2, md: 2, lg: 2, xl: 4 }} alignSelf="flex-start">
                                 {
                                     selectedVariant?.images?.map((v) => (
                                         <Grid size={12}>
                                             <Box
                                                 className="detailimg-box"
-                                                sx={{ width: '100%', height: '97px', border: selectedImage === v ? "2px solid black" : "1px solid #ccc", }}
+                                                sx={{ width: '100%', height: { xs: '58px', sm: '58px', md: '70px', lg: '97px', xl: '120px' }, border: selectedImage === v ? "2px solid black" : "1px solid #ccc", }}
                                                 onClick={() => setSelectedImage(v)}
                                             >
                                                 <img src={IMG_URL + v} alt="no" className="demoimg" />
@@ -161,7 +162,7 @@ function Productdetail() {
                                 </Grid> */}
                             </Grid>
                             <Grid size={{ xs: 9, sm: 8, md: 9 }} display="flex" flexDirection='column'>
-                                <Box className="detailimg-box deatail-main-img" sx={{ height: detailproduct?.variants?.length > 1 ? '600px' : '100%' }}>
+                                <Box className="detailimg-box deatail-main-img" sx={{ height: { xs: '100%', sm: '75%', md: '75%', lg: '100%' } }}>
                                     {/* <img src="../../../public/assets/images/productdetail/image 63.png" alt="" width='100%' /> */}
                                     <img
                                         src={
@@ -222,7 +223,7 @@ function Productdetail() {
 
                             <Typography variant="h5" sx={{ mt: { xs: 2, sm: 1, md: 2 }, mb: { xs: 2, sm: 1, md: 3 }, fontSize: { xs: '20px', sm: '20px', md: '24px' } }}>₹{detailproduct?.price}</Typography>
 
-                            <Typography sx={{ fontSize: '14px', maxWidth: '373px', mb: { xs: 3, sm: 2, md: 3 } }}>
+                            <Typography sx={{ fontSize: '14px', mb: { xs: 3, sm: 2, md: 3 } }}>
                                 PlayStation 5 Controller Skin High quality vinyl with air channel adhesive for easy bubble free install & mess free removal Pressure sensitive.
                             </Typography>
 
@@ -300,16 +301,16 @@ function Productdetail() {
                             </label> */}
 
                             {/* size */}
-                            <Box sx={{ display: 'flex', alignItems: 'center', gap: 2, mt: { xs: 3, sm: 1, md: 3 } }}>
+                            <Box sx={{ display: 'flex', alignItems: 'center', gap: 2, mt: { xs: 3, sm: 2, md: 3 } }}>
                                 <Typography sx={{ fontSize: { sm: '18px', md: '20px' } }}>Size :</Typography>
 
                                 <Box sx={{ display: 'flex', gap: 1 }}>
                                     {
                                         selectedVariant?.size?.map((v) => {
 
-                                          return ( v === 'Free_Size' ? <Typography className="fsizebox">{v}</Typography> : <Typography className="sizebox">{v}</Typography> )
+                                            return (v === 'Free_Size' ? <Typography className="fsizebox">{v}</Typography> : <Typography className="sizebox">{v}</Typography>)
                                         }
-                                       )
+                                        )
                                     }
                                 </Box>
                             </Box>
@@ -318,7 +319,7 @@ function Productdetail() {
                             <Box sx={{ display: 'flex', mt: { xs: 3, sm: 2, md: 3 }, gap: { xs: 3, sm: 2, lg: 4 } }} className="counter-box">
                                 <Box className='countbox'>
                                     <button className="count-btn" style={{ borderRight: 'solid 1px rgb(172, 167, 167)', backgroundColor: active === 'decrese' ? '#DB4444' : 'white', color: active === 'decrese' ? 'white' : 'black' }} onClick={handleDecrese}>-</button>
-                                    <Typography sx={{ padding: { xs: '0 35px', sm: '0 15px', lg: '0 35px' } }} className="counter-no">{counter}</Typography>
+                                    <Typography sx={{ padding: { xs: '0 35px', sm: '0 20px', md: '0 32px', lg: '0 35px' } }} className="counter-no">{counter}</Typography>
                                     <button className="count-btn" style={{ borderLeft: 'solid 1px rgb(172, 167, 167)', backgroundColor: active === 'increse' ? '#DB4444' : 'white', color: active === 'increse' ? 'white' : 'black' }} onClick={handleIncrese}>+</button>
                                 </Box>
 
